df86ca6107a5b276cd1ef3348d048c62b85fc795
Brian Leonard <brian.leonard@mongodb.com>
2018-12-04 18:50:45 -0500

    Params template

---
+++ params.py
@@ -0,0 +1,10 @@
+# Input parameters
+
+# Atlas API
+project_id = '<Project ID found under Atlas Project Settings>'
+user = '<User Name>'
+password = '<API Key>'
+
+# Atlas DB
+conn_string = '<Application connection string provided by the Atlas UI>'
+database = 'analysis'
